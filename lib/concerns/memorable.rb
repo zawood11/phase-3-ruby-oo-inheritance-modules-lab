a7cf8d2b2ed0de54c6e5f82841d17630aae2800f
Zach <zawood11@gmail.com>
2022-03-03 23:58:37 -0500

    commit tree 1b27252007bd970e314675026041b53a390a4359

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+module Memorable
+    module ClassMethods
+      def reset_all
+          self.all.clear
+        end
+      
+        def count
+          self.all.count
+        end    
+    end
+  end
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 require 'pry'
+require_relative './concerns/memorable.rb'
 
 class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  
   @@songs = []
 
   def initialize
@@ -18,13 +21,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
